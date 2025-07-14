e715367a6f2f139aa093c84d64e77a922916f72e
Moiz Mansoor Ali <moizmansooraliis@gmail.com>
2025-07-14 16:27:58 +0530

    Add a new nvim tree command shortcut

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -23,5 +23,6 @@ return {
 
         -- Add keymap to toggle file tree
         vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
+        vim.keymap.set("n", "<leader>ft", ":NvimTreeFindFile<CR>", { silent = true })
     end,
 }
