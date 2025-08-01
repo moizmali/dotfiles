7a04ffe423376497069f1a6df5e0c7664db0ea94
Moiz Mansoor Ali <moizmansooraliis@gmail.com>
2025-08-01 14:22:47 +0530

    Add new keymap for automatically generating d2 diagrams

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -16,14 +16,14 @@ keymap.set("n", "<leader>q", ":q<CR>")
 -- Split vertically & horizontally
 keymap.set("n", "<leader>sv", "<C-w>v<C-w>l", { desc = "Split Vertically" })
 keymap.set("n", "<leader>sh", "<C-w>s<C-w>j", { desc = "Split Horizontally" })
-keymap.set("n", "<leader>l", "<C-w>l")
-keymap.set("n", "<leader>h", "<C-w>h")
-keymap.set("n", "<leader>j", "<C-w>j")
-keymap.set("n", "<leader>k", "<C-w>k")
-keymap.set("n", "<leader><", "<C-w><")
-keymap.set("n", "<leader>>", "<C-w>>")
+keymap.set("n", "<leader>l", "<C-w>l", { desc = "Focus Right Window" })
+keymap.set("n", "<leader>h", "<C-w>h", { desc = "Focus Left Window" })
+keymap.set("n", "<leader>j", "<C-w>j", { desc = "Focus Bottom Window" })
+keymap.set("n", "<leader>k", "<C-w>k", { desc = "Focus Top Window" })
 
 -- Move highlited text up and down
-keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected text down" })
+keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected text up" })
 
+-- Generate D2 diagrams
+keymap.set("n", "<leader>d2", ":!d2 %<CR>", { desc = "Run d2 on current buffer" })
