9b0efff69da8a69c648d14459317d66eaaba4ba8
Moiz Mansoor Ali <moizmansooraliis@gmail.com>
2025-12-06 09:24:34 +0530

    Update the jq keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -30,4 +30,4 @@ keymap.set("n", "<leader>d2", ":!d2 %<CR>", { desc = "Run d2 on current buffer" 
 
 -- Run jq on highlighted text
 keymap.set("v", "<leader>jq", ":!jq .<CR>", { desc = "Run jq on selected text" })
-keymap.set("v", "<leader>jq", ":!jq -c .<CR>", { desc = "Run 'jq -c' on selected text" })
+keymap.set("v", "<leader>jc", ":!jq -c .<CR>", { desc = "Run 'jq -c' on selected text" })
