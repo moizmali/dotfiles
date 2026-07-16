12da5a15490d08ab1613fa0d9f85e5feb3b0c16e
Moiz Mansoor Ali <moizmansooraliis@gmail.com>
2026-07-16 11:18:06 +0530

    feature(nsesh): Update note session script functionality

--- scripts/nsesh.sh
+++ scripts/nsesh.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 notes_dir=Notes
-date_file=`date +%F`
 
 mkdir -p ~/$notes_dir
-touch ~/$notes_dir/$date_file.md
 
 if tmux has-session -t $notes_dir; then
     tmux attach-session -t $notes_dir
 else
-    tmux new-session -s $notes_dir -c ~/$notes_dir nvim $date_file.md
+    tmux new-session -d -s $notes_dir -c ~/$notes_dir -n nvim 'nvim .'
+    tmux new-window -t $notes_dir -n termial
+    tmux attach-session -t $notes_dir:nvim
 fi
